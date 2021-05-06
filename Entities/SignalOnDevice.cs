bfb6ab57f6f12a348545f2df2ad33dea6f6e11b0
pmcwalrus <pmcwalrus@gmail.com>
2021-05-06 14:29:31 +0300

    Добавлен класс для хранения элементов раскладки

---
+++ Entities/SignalOnDevice.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Nti.XlsxReader.Entities
+{
+    public class SignalOnDevice : INotifyPropertyChanged
+    {
+
+        private string _index;
+        public string Index
+        {
+            get => _index;
+            set
+            {
+                _index = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _numberStr;
+        public string NumberStr
+        {
+            get => _numberStr;
+            set
+            {
+                _numberStr = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region PropertyChanged Impllementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
