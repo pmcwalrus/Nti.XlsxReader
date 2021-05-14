a3747d6c443528dbdae9989acfad389c20bbbf6c
pmcwalrus <pmcwalrus@gmail.com>
2021-05-14 13:38:08 +0300

    Добавлены сеттеры в заголовки.

--- Types/Headers.cs
+++ Types/Headers.cs
@@ -4,20 +4,20 @@
     {
         #region Main Signal List Headers
 
-        public static string DescriptionHeader {get;} = "Наименование параметра";
-        public static string IndexHeader { get; } = "Индекс параметра";
-        public static string UnitsHeader { get; } = "Единицы измерения";
-        public static string SetpointsTypeHeader { get; } = "Тип уставки";
-        public static string SetpointValuesHeader { get; } = "Значение";
-        public static string DelayTimeHeader { get; } = "Время задержки,с";
-        public static string InversionHeader { get; } = "Инверсия";
-        public static string SystemIdHeader { get; } = "System ID";
-        public static string SignalIdHeader { get; } = "Signal ID (new)";
-        public static string SignalTypeHeader { get; } = "Type";
-        public static string PstsHeader { get; } = "ПСТС";
-        public static string ShmemHeader { get; } = "shmem";
-        public static string UpsHeader { get; } = "УПС";
-        public static string SignalTypeTextHeader { get; } = "Тип сигнала";
+        public static string DescriptionHeader { get; set; } = "Наименование параметра";
+        public static string IndexHeader { get; set; } = "Индекс параметра";
+        public static string UnitsHeader { get; set; } = "Единицы измерения";
+        public static string SetpointsTypeHeader { get; set; } = "Тип уставки";
+        public static string SetpointValuesHeader { get; set; } = "Значение";
+        public static string DelayTimeHeader { get; set; } = "Время задержки,с";
+        public static string InversionHeader { get; set; } = "Инверсия";
+        public static string SystemIdHeader { get; set; } = "System ID";
+        public static string SignalIdHeader { get; set; } = "Signal ID (new)";
+        public static string SignalTypeHeader { get; set; } = "Type";
+        public static string PstsHeader { get; set; } = "ПСТС";
+        public static string ShmemHeader { get; set; } = "shmem";
+        public static string UpsHeader { get; set; } = "УПС";
+        public static string SignalTypeTextHeader { get; set; } = "Тип сигнала";
 
         #endregion
 
